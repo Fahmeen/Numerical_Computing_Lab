d8e6b768f5d45bdbc9e18152d85a6596da92a5cd
_ <fahmeenmazhar@gmail.com>
2020-03-29 23:04:11 +0500

    Update RegulaFalsiMethod.py

--- RegulaFalsiMethod/RegulaFalsiMethod.py
+++ RegulaFalsiMethod/RegulaFalsiMethod.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Mar 29 20:21:02 2020
-
 @author: user
 """
 
@@ -17,17 +16,14 @@ def regulaFalsi(x0,x1,e):
     while condition:
         x2 = x0 - (x1-x0) * f(x0)/( f(x1) - f(x0) )
         print('Iteration=%d, x2 = %0.6f and f(x2) = %0.6f' % (step, x2, f(x2)))
-
         if f(x0) * f(x2) < 0:
             x1 = x2
         else:
             x0 = x2
-
         step = step + 1
         condition = abs(f(x2)) > e
-
+        
     print('\nRequired root is: %0.8f' % x2)
-
 
 # Input Section
 x0 = input('First Guess: ')
